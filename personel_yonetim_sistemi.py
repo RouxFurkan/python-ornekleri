a71236fd3084edcee09df474ce42ca3415f84c8b
Furkan Y. <145954104+RouxFurkan@users.noreply.github.com>
2024-12-30 23:41:16 +0300

    Update personel_yonetim_sistemi.py

--- personel_yonetim_sistemi.py
+++ personel_yonetim_sistemi.py
@@ -2,103 +2,84 @@
 
 #İçeride kayıtlı kullanıcılar
 
-personel_ahmet = {
-    "isim" : "Ahmet",
-    "soyisim" : "Karalar",
-    "brans" : "Full Stack Developer",
-}
+personeller = {
+    "ahmet": {"isim" : "ahmet"},
 
-personel_mehmet = {
-    "isim" : "Mehmet",
-    "soyisim" : "Sudan",
-    "brans" : "Backend developer",
-}
 
-personel_furkan = {
-    "isim" : "Furkan",
-    "soyisim" : "Yesilirmak",
-    "brans" : "Frontend Developer",
-}
 
-personel_enes = {
-    "isim" : "Enes",
-    "soyisim" : "Isıl",
-    "brans" : "Dev OPS",
 }
-
-personel_kerem = {
-    "isim" : "Kerem",
-    "soyisim" : "Anıl",
-    "brans" : "Senior",
-}
-
 
 #Önceden tanımlanmış kullanıcı
-kullanici_adi = "furkan"
-sifre = "5223"
+kullanici_adi = "1"
+sifre = "1"
 
-#Mevcutta bir hesabı oldup olmadığını öğrenme
+#Mevcutta bir hesabı olup olmadığını öğrenme
 while True:
-    hesap_bilgisi = input("Mevcutta bir hesabınız var mı?\nVarsa, Evet, yoksa Hayır yazın. :")
+    hesap_bilgisi = input("Mevcutta bir hesabınız var mı?\nVarsa, Evet yoksa Hayır yazın. :")
 
     if hesap_bilgisi == "Evet" or hesap_bilgisi == "Hayır":
         break
     else:
-        print("lütfen belirtilen formatta yazınız")
+        print(f"lütfen belirtilen formatta yazınız\nyazdığınız hatalı format: {hesap_bilgisi}\n")
         continue
 
-
-
-#Mevcutta kayıtlı bir hesabı yoksa yeni hesap oluşturma
+#Yeni hesap olusturma veya direkt login olma
 while True:
+    if hesap_bilgisi == "Evet":
+        kullanici_adi_girin = input("Lütfen kullanıcı adınızı giriniz: ")
+        sifre_girin = input("Lütfen şifrenizi giriniz: ")
+        if kullanici_adi == kullanici_adi_girin and sifre == sifre_girin:
+            print("giriş başarılı")
+            break
+        else:
+            print("yanlış kullanıcı adı veya şifre\n")
+            continue
+    elif hesap_bilgisi == "Hayır":
+            yeni_kullanici_girdi = False
+            yeni_kullanici_adi_giris = input("Yeni bir kullanıcı adı oluşturun: ")
+            yeni_sifre_giris = input("Yeni bir şifre oluşturun: ")
+            print(f"Hesabınız başarıyla oluşturuldu\n Kullanıcı adınız: {yeni_kullanici_adi_giris}\n Şifreniz: {yeni_sifre_giris}\n")
 
-    if hesap_bilgisi == "Hayır":
-        yeni_kullanici_adi_giris = input("Yeni bir kullanıcı adı oluşturun: ")
-        yeni_sifre_giris = input("Yeni bir şifre oluşturun: ")
-        print(f"Hesabınız başarıyla oluşturuldu\n Kullanıcı adınız: {yeni_kullanici_adi_giris}\n Şifreniz: {yeni_sifre_giris}")
-        break
-    else:
-        break
 
-
-
-#Varsa ya da yeni oluşturulduysa login olma
-while True:
-    kullanici_adi_girin = input("Lütfen kullanıcı adınızı giriniz: ")
-    sifre_girin = input("Lütfen şifrenizi giriniz: ")
-    if kullanici_adi == kullanici_adi_girin and sifre == sifre_girin or yeni_kullanici_adi_giris == kullanici_adi_girin and yeni_sifre_giris == sifre_girin:
-        print("giriş başarılı")
-        break
-    else:
-        print("yanlış kullanıcı adı veya şifre\n")
-
+            while not yeni_kullanici_girdi:
+                kullanici_adi_girin = input("Lütfen kullanıcı adınızı giriniz: ")
+                sifre_girin = input("Lütfen şifrenizi giriniz: ")
+                if kullanici_adi_girin == yeni_kullanici_adi_giris and sifre_girin == yeni_sifre_giris:
+                    yeni_kullanici_girdi = True
+                    print("giriş başarılı")
+                    break
+                else:
+                    print("yanlış kullanıcı adı veya şifre\n")
+                    continue
+            if yeni_kullanici_girdi:
+                break
 
 #personel bilgilerini sorgulama
 def personel_sorgulama():
 
-    while True:
-        personel_bilgi_alma = input("Hangi personelin bilgisini öğrenmek istiyorsunuz? : ")
 
-        if personel_bilgi_alma in ["ahmet","mehmet","furkan","enes","kerem"]:
-            if personel_bilgi_alma == "ahmet":
-                print(personel_ahmet)
-                break
-            if personel_bilgi_alma == "mehmet":
-                print(personel_mehmet)
-                break
-            if personel_bilgi_alma == "furkan":
-                print(personel_furkan)
-                break
-            if personel_bilgi_alma == "enes":
-                print(personel_enes)
-                break
-            if personel_bilgi_alma == "kerem":
-                print(personel_kerem)
-                break
+    while True:
+        yapilmak_istenen_islem = input("Hangi işlemi yapmak istiyorsunuz? örneğin 'personel ekleme' veya 'personel bilgi alma' gibi:  ")
+        if yapilmak_istenen_islem in ["personel ekleme", "personel bilgi alma"]:
+            break
         else:
-            print("Lütfen gecerli bir isim giriniz")
+            print("lütfen belirtilen şekilde işlemi belirtiniz")
             continue
 
-    print("dogru isim girildi")
+    while True:
+        if yapilmak_istenen_islem in ["personel bilgi alma"]:
+            personel_bilgi_alma = input("Hangi personelin bilgisini öğrenmek istiyorsunuz? (çıkmak için 'çık' yazınız) : ")
+            if personel_bilgi_alma in personeller:
+                print(personeller[personel_bilgi_alma])
+                continue
+            elif personel_bilgi_alma == "çık":
+                quit()
+            else:
+                print("Lütfen gecerli bir isim giriniz\n")
+                continue
+        else:
+            break
+
+
 
 personel_sorgulama()
